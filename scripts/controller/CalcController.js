918a648771b55886a57cccc2c3b217cb2687eba9
Wladson Ramos <wladsonramossi@gmail.com>
2022-10-15 04:39:52 -0300

    creating methods for operations

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor(){
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#date");
@@ -33,6 +34,127 @@ class CalcController {
 
     }
 
+    clearAll(){
+
+        this._operation = [];
+    }
+
+    cancelEntry(){
+
+        this._operation.pop();
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+
+    }
+
+    getLastOperation(){
+
+        return this._operation[this._operation.length-1];
+
+    }
+
+    setLastOperation(value){
+
+        this._operation[this._operation.length-1] = value;
+
+    }
+
+    isOperator(value){
+
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1);
+
+    }
+
+    addOperation(value){
+
+        if(isNaN(this.getLastOperation())){
+
+            if(this.isOperator(value)){
+
+                this.setLastOperation(value);
+
+            }else if(isNaN(value)){
+
+                console.log(value);
+
+            }
+            else{
+
+                this._operation.push(value);
+
+            }
+        } else{
+
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue)); 
+
+        }
+
+        console.log(this._operation);
+    }
+
+    execBtn(value){
+
+        switch (value){
+
+            case 'ac':
+                this.clearAll();
+                break; 
+
+            case 'ce':
+                this.cancelEntry();
+
+            case 'soma':
+                this.addOperation('+');
+                break;
+
+            case 'subtracao':
+                this.addOperation('-');
+                break;
+
+            case 'divisao':
+                this.addOperation('/');
+                break;
+
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+
+            case 'porcento':
+                this.addOperation('%');
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case 'ponto':
+                this.addOperation('.');
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
+
+    }
+
     initButtonsEvents(){
         
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
@@ -41,7 +163,11 @@ class CalcController {
 
             this.addEventListenerAll(btn, "click drag", e => {
 
-                console.log(btn.className.baseVal.replace("btn-",""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
+
+                
         
             });
 
