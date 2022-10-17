1f2e0ec3e2f04e0049be2f62c43a1c3172a1492c
Wladson Ramos <wladsonramossi@gmail.com>
2022-10-17 03:00:46 -0300

    calculating result and updating the display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -68,6 +68,48 @@ class CalcController {
 
     }
 
+    pushOperation(value){
+
+        this._operation.push(value);
+
+        if(this._operation.length > 3){
+
+            this.calc();
+
+        }
+
+    }
+
+    calc(){
+
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
+        
+    }
+
+    setLastNumberToDisplay(){
+
+        let lastNumber;
+
+        for(let i = this._operation.length-1;i >= 0;i--){
+
+            if(!this.isOperator(this._operation[i])){
+
+                lastNumber = this._operation[i];
+                break;
+
+            }       
+        }
+
+        this.displayCalc = lastNumber;
+
+    }
+
     addOperation(value){
 
         if(isNaN(this.getLastOperation())){
@@ -78,22 +120,35 @@ class CalcController {
 
             }else if(isNaN(value)){
 
-                console.log(value);
+                console.log('Outra coisa', value);
 
             }
             else{
 
-                this._operation.push(value);
+                this.pushOperation(value);
+
+                this.setLastNumberToDisplay();
 
             }
         } else{
 
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue)); 
+            if(this.isOperator(value)){
+
+                this.pushOperation(value);
+
+            }
+            else{
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue)); 
+
+                this.setLastNumberToDisplay();
+                
+            }
+
 
         }
 
-        console.log(this._operation);
     }
 
     execBtn(value){
